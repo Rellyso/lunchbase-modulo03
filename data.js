2fdd2d5c93719c6981b90132005e2d2f36124d80
Rellyson <rellysondouglas2015@gmail.com>
2020-06-24 09:53:06 -0300

    cards data

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "GIT e GITHUB na prática | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "TI4v4Y8yRjw",
+        title: "Styles Grid Javascript | Mayk Brito",
+        duration: "12 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS GRID layout na prática | Mayk Brito",
+        duration: "37 min",
+        price: "Free"
+    },
+    {
+        id: "rAzHvYnQ8DY",
+        title: "Array: high order functions | Mayk Brito",
+        duration: "54 min",
+        price: "Free"
+    },
+    {
+        id: "c7P03kkrEG8",
+        title: "Configurando o VSCode para Node.js, React.js e React Native | Mayk Brito",
+        duration: "22 min",
+        price: "Free"
+    },
+]
