ce3ea5e6931965bf85b0ce0131e11b2a8a462800
Rellyson <rellysondouglas2015@gmail.com>
2020-06-24 10:20:23 -0300

    adding dynamic data on about page

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ server.set('view engine', 'njk')
 //configurando o nunjucks passando o nome da pasta que será utlizada
 // o template engine (views)
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 //pegar do server a / e retonar uma função com requisição e resposta  
@@ -22,7 +23,19 @@ server.get('/', function (req, res) {
     // return res.send('Hi! How are you?')
 
     //retorna a renderização da página index
-    return res.render('about')
+    const about = {
+        img_url: "https://avatars2.githubusercontent.com/u/63888510?s=460&u=a435f843937d4f52e261df7a9578bc5bb2acf449&v=4",
+        name: "Rellyson Douglas",
+        role: "Estudante - Rocketseat",
+        description: 'Desenvolvedor web fullstack em aprendizagem. Aluno da <a href="http://rocketseat.com.br" target="_blank">Rocketseat</a>',
+        links: [
+            {name: "Github", url: "https://github.com/rellyso/"},
+            {name: "Twitter", url: "https://twitter.com/rellyson1/"},
+            {name: "Linkedin", url: "https://linkedin.com/in/rellyson-douglas/"}
+        ]
+    }
+
+    return res.render('about', {about})
 })
 
 server.get('/portfolio', function (req, res) {
